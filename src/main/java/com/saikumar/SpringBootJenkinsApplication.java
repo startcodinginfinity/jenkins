8217963b977e8ea2b98d3c533d1781c6d6f651ca
saikumar <venkatasaikumar803@gmail.com>
2025-02-15 18:10:59 -0600

    first commit

---
+++ src/main/java/com/saikumar/SpringBootJenkinsApplication.java
@@ -0,0 +1,30 @@
+package com.saikumar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	
+	
+	@PostConstruct
+	public void init() {
+		
+		logger.info("Application started .......");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		logger.info("Application Executed .......");
+		
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+	}
+
+}
